0a01f9e76e1dd68ed7b8c0ffd72e6ef40096e94f
Albertino Neto <albeneto72@gmail.com>
2025-10-01 23:09:20 -0300

    Inicio de projeto.

---
+++ src/main/java/br/com/albeneto/spring_batch_app/SpringBatchAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.albeneto.spring_batch_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/albeneto/spring_batch_app/SpringBatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.albeneto.spring_batch_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
